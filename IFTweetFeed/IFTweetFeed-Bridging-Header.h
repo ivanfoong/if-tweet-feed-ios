0553b43ec8bdb8c0bd4d23a6ff65de86ba04e8c3
Ivan Foong <vonze21@gmail.com>
2017-12-12 11:24:48 +0800

    adding twitterkit integration

---
+++ IFTweetFeed/IFTweetFeed-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  IFTweetFeed-Bridging-Header.h
+//  IFTweetFeed
+//
+//  Created by Ivan Foong on 12/12/17.
+//  Copyright © 2017 Ivan Foong. All rights reserved.
+//
+
+#ifndef IFTweetFeed_Bridging_Header_h
+#define IFTweetFeed_Bridging_Header_h
+
+#import <TwitterKit/TwitterKit.h>
+
+#endif /* IFTweetFeed_Bridging_Header_h */
