3e368213875726c06b2de7ee92296a5186cf0243
Ivan Foong <vonze21@gmail.com>
2017-12-12 13:41:43 +0800

    renamed ViewController to LoginViewController

--- IFTweetFeed/LoginViewController.swift
+++ IFTweetFeed/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  IFTweetFeed
 //
 //  Created by Ivan Foong on 12/12/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
